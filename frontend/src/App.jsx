0289d3b7393ba11e31dc32072d2a9fe203698dcf
Yazky Maulana <yazki.maulana@gmail.com>
2025-04-18 15:24:57 +0700

    feat: initial setup project (Laravel + React+Vite + Tailwind)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex justify-center items-center flex-col-reverse'>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 className='text-3xl font-bold text-slate-400'>Simplee Commerce In Progress...</h1>
+    </>
+  )
+}
+
+export default App
